409d55cb027e9f6e36cbbe5b1845c154f9b919a5
i-kogda-nibud-nas-ne-budet <dansparrow32@gmail.com>
2023-03-06 02:31:42 +0300

    0303

---
+++ 0303.py
@@ -0,0 +1,34 @@
+from turtle import*
+image='''
+01010101
+10101010
+01010101
+10101010
+01010101
+10101010
+01010101
+10101010'''
+def pixel(lenght,my_color):
+  sq(lenght,my_color)
+  forward(lenght)
+def sq(lenght,my_color):
+  begin_fill()
+  color(my_color)
+  for i in range(4):
+    forward(lenght)
+    left(90)
+  end_fill()
+speed(0)
+amount_string=0
+for i in image:
+  if i=='0':
+    pixel(20,'black')
+  if i=='1':
+    pixel(20,'cyan')
+  if i=='\n':
+    amount_string+=1
+    goto(0,amount_string*(-20))
+
+exitonclick()
+
+# 2 random number
--- 2402.py
+++ 2402.py
@@ -19,12 +19,24 @@ def cube(lenght,angle):
     goto(0,0)
     left(90+angle)
 
-width(5)
-color('cyan')
-cube(200,80)
-color('red')
-cube(200,70)
-color('orange')
-cube(200,60)
+#width(5)
+#color('cyan')
+#cube(200,80)
+#color('red')
+#cube(200,70)
+#color('orange')
+#cube(200,60)
+
+def square(lenght,my_color):
+    begin_fill()
+    color(my_color)
+    for i in range(4):
+      forward(lenght)
+      left(90)
+    end_fill()
+
+square(20,'red')
+forward(20)
+square(20,'cyan')
 
 exitonclick()
